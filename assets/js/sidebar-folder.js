d288d0ba52a1bb794dbc18c44e5858c6a2ad56b1
chosunmin <lllvvv10@gmail.com>
2024-12-18 15:32:44 +0900

    test

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,21 +1,17 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-  // 모든 'has-submenu' 요소에 클릭 이벤트 추가
+  // 상위 메뉴 클릭 이벤트 추가
   document.querySelectorAll('.has-submenu > a').forEach(function (menuLink) {
     menuLink.addEventListener('click', function (event) {
       event.preventDefault(); // 기본 링크 동작 방지
-      
       const submenu = this.nextElementSibling;
 
-      // 서브메뉴 슬라이드 토글
+      // 서브메뉴 토글
       if (submenu.classList.contains('open')) {
         submenu.classList.remove('open'); // 슬라이드 업
       } else {
-        // 다른 열린 서브메뉴 닫기
         document.querySelectorAll('.submenu.open').forEach(function (openSubmenu) {
-          openSubmenu.classList.remove('open');
+          openSubmenu.classList.remove('open'); // 다른 열린 서브메뉴 닫기
         });
-
         submenu.classList.add('open'); // 슬라이드 다운
       }
     });
